57450e2fb26f78924ef024eaed8804685a2c0d5c
zhuoxiao yang <91831716+yangtianwang@users.noreply.github.com>
2021-12-02 14:25:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-#include <iostream>
-
+#include<iostream>
+#include"lib.h"
+using namespace std;
 int main(){
-  return 0;
+  int n=0;
+  cin>>n;
+  if(numerip(n,n-1)){
+    cout<<"numero primo";
+  }else{
+    cout<<"numero non primo";
+  }
+  
+  return 0:
 }
+
